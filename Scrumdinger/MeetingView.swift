83d621dfa5e9c96f2bda631b2b77779feb4e0db2
martapanc <marta.panc@gmail.com>
2022-04-08 20:38:26 +0100

    Add gitignore and ProgressView

--- Scrumdinger/MeetingView.swift
+++ Scrumdinger/MeetingView.swift
@@ -9,8 +9,19 @@ import SwiftUI
 
 struct MeetingView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            ProgressView(value: 5, total: 15)
+            HStack {
+                VStack {
+                    Text("Seconds Elapsed")
+                    Label("300", systemImage: "hourglass.bottomhalf.fill")
+                }
+                VStack {
+                    Text("Seconds Remaining")
+                    Label("300", systemImage: "hourglass.tophalf.fill")
+                }
+            }
+        }
     }
 }
 
