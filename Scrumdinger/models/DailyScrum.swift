50ca11c65163f553218e7d6db0861c2feb4be3bc
martapanc <marta.panc@gmail.com>
2022-04-10 20:27:45 +0100

    Add Navigation Links

--- Scrumdinger/models/DailyScrum.swift
+++ Scrumdinger/models/DailyScrum.swift
@@ -7,16 +7,28 @@ import Foundation
 struct DailyScrum: Identifiable {
     let id: UUID
     var title: String
-    var attendees: [String]
+    var attendees: [Attendee]
     var lengthInMinutes: Int
     var theme: Theme
 
     init(id: UUID = UUID(), title: String, attendees: [String], lengthInMinutes: Int, theme: Theme) {
         self.id = id
         self.title = title
-        self.attendees = attendees
+        self.attendees = attendees.map { Attendee(name: $0) }
         self.lengthInMinutes = lengthInMinutes
         self.theme = theme
+    }
+}
+
+extension DailyScrum {
+    struct Attendee: Identifiable {
+        let id: UUID
+        var name: String
+
+        init(id: UUID = UUID(), name: String) {
+            self.id = id
+            self.name = name
+        }
     }
 }
 
@@ -24,7 +36,7 @@ extension DailyScrum {
     static let sampleData: [DailyScrum] =
             [
                 DailyScrum(title: "Designers", attendees: ["Gianvincenzo", "Ignazio", "Gertrude", "Tarcisio"], lengthInMinutes: 10, theme: .yellow),
-                DailyScrum(title: "Devs", attendees: ["Ettore", "Cunegonda", "Genoveffa"], lengthInMinutes: 10, theme: .orange),
-                DailyScrum(title: "Testers", attendees: ["Abbondio", "Giovanni Maria", "Bradamante", "Grimilde"], lengthInMinutes: 10, theme: .poppy)
+                DailyScrum(title: "Devs", attendees: ["Ettore", "Cunegonda", "Genoveffa"], lengthInMinutes: 8, theme: .orange),
+                DailyScrum(title: "Testers", attendees: ["Abbondio", "Giovanni Maria", "Bradamante", "Grimilde"], lengthInMinutes: 13, theme: .poppy)
             ]
 }
