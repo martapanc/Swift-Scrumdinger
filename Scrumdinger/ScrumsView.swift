f4442806ca481160a164bb1cb03ac46c6e39dc7d
martapanc <marta.panc@gmail.com>
2022-04-10 18:32:19 +0100

    Add ScrumsView

---
+++ Scrumdinger/ScrumsView.swift
@@ -0,0 +1,25 @@
+//
+// Created by Marta Pancaldi on 10/04/2022.
+//
+
+import Foundation
+import SwiftUI
+
+struct ScrumsView: View {
+    let scrums: [DailyScrum]
+
+    var body: some View {
+        List {
+            ForEach(scrums) { scrum in
+                CardView(scrum: scrum)
+                        .listRowBackground(scrum.theme.mainColor)
+            }
+        }
+    }
+}
+
+struct ScrumsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrumsView(scrums: DailyScrum.sampleData)
+    }
+}
--- Scrumdinger/models/Theme.swift
+++ Scrumdinger/models/Theme.swift
@@ -2,7 +2,6 @@
 // Created by Marta Pancaldi on 08/04/2022.
 //
 
-import Foundation
 import SwiftUI
 
 enum Theme: String {
